bd36bdc215a80ae8cdfe02bdc0c794752c8050ab
Horia Racoviceanu <horia@racoviceanu.com>
2017-06-18 14:45:05 -0400

    Add useful scripts for web servers

---
+++ src/usr/local/bin/get-ocsp.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+SITE="$1"
+LEDIR="/etc/ssl/acme"
+DIR="$LEDIR"
+HOST="ocsp.int-x3.letsencrypt.org"
+SITE="freedns.afraid.org"
+
+# Get chain signed by ISRG Root X1 https://letsencrypt.org/certificates/
+ftp -a -o chain.pem https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt
+
+# Get OCSP response
+openssl ocsp -no_nonce \
+             -respout      $DIR/$SITE.ocsp.resp.new \
+             -issuer       $DIR/chain.pem \
+             -verify_other $DIR/chain.pem \
+             -cert         $DIR/$SITE.crt \
+             -url http://$HOST/ \
+             -header "HOST" "$HOST" > $DIR/$SITE.ocsp.reply.txt 2>&1
+
+if grep -q ": good" $DIR/$SITE.ocsp.reply.txt; then
+    if cmp -s $DIR/$SITE.ocsp.resp.new $DIR/$SITE.ocsp.resp.der; then
+        rm $DIR/$SITE.ocsp.resp.new
+    else
+        mv $DIR/$SITE.ocsp.resp.new $DIR/$SITE.ocsp.resp.der
+        rcctl restart httpd
+    fi
+else
+    cat $DIR/$SITE.ocsp.reply.txt
+fi
+
+mv $DIR/$SITE.ocsp.reply.txt $DIR/$SITE.ocsp.reply.old.txt
+
+# Verify
+#openssl s_client -connect $SITE:443 -tlsextdebug -status | grep OCSP
